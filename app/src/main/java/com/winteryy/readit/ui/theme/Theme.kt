869a04407c042f6984b825eb8a3145a5069971ae
Lee HuiGwang <lhk9805@gmail.com>
2024-08-11 03:12:46 +0900

    design: 테마 값 임시로 할당
    issue: #1

--- app/src/main/java/com/winteryy/readit/ui/theme/Theme.kt
+++ app/src/main/java/com/winteryy/readit/ui/theme/Theme.kt
@@ -18,9 +18,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = theme_color_dodgerBlue,
+    secondary = theme_color_lightDodgerBlue,
+    tertiary = theme_color_malibu
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
