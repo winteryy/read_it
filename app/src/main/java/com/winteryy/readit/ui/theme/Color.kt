d658761a530b94930977f51b38c060c5475758e8
Lee HuiGwang <lhk9805@gmail.com>
2024-08-11 03:09:16 +0900

    design: 기본 테마 컬러값 설정
    issue: #1

--- app/src/main/java/com/winteryy/readit/ui/theme/Color.kt
+++ app/src/main/java/com/winteryy/readit/ui/theme/Color.kt
@@ -2,6 +2,17 @@ package com.winteryy.readit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val theme_color_dodgerBlue = Color(0xFF0583F2)
+val theme_color_lightDodgerBlue = Color(0xFF0597F2)
+val theme_color_malibu = Color(0xFF57B7F2)
+
+val theme_grey_black = Color(0xFF000000)
+val theme_grey_gunPowder = Color(0xFF565659)
+val theme_grey_whiteSmoke = Color(0xFFF2F2F2)
+val theme_grey_white = Color(0xFFFFFFFF)
+
+// initial color
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
