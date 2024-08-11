ca658eb064a77e77008f124fcf770af1d2522f00
Lee HuiGwang <lhk9805@gmail.com>
2024-08-12 02:35:32 +0900

    chore: Navigation dependency 추가
    issue: #3

---
+++ app/src/main/java/com/winteryy/readit/ui/ReadItApp.kt
@@ -0,0 +1,11 @@
+package com.winteryy.readit.ui
+
+import androidx.compose.runtime.Composable
+import com.winteryy.readit.ui.theme.ReadItTheme
+
+@Composable
+fun ReadItApp() {
+    ReadItTheme {
+        val navController = remember
+    }
+}
