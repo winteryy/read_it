50a853faa3c33da9176e9ec7769309eaed2e47c6
Lee HuiGwang <lhk9805@gmail.com>
2024-08-16 17:55:03 +0900

    feat: RoomModule 및 ReadItDatabase 구현
    issue: #7

---
+++ app/src/main/java/com/winteryy/readit/data/local/ReadItDatabase.kt
@@ -0,0 +1,11 @@
+package com.winteryy.readit.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.winteryy.readit.data.local.bookstorage.BookDao
+import com.winteryy.readit.data.local.bookstorage.BookEntity
+
+@Database(entities = [BookEntity::class], version = 1)
+abstract class ReadItDatabase(): RoomDatabase() {
+    abstract fun bookDao(): BookDao
+}
