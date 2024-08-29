5d6ca2fcbdbbb710ce9c335fd900b0d8c0fa68a9
Lee HuiGwang <lhk9805@gmail.com>
2024-08-30 00:15:52 +0900

    remove: 불필요 import 삭제
    issue: #9

--- app/src/main/java/com/winteryy/readit/ui/home/HomeSearchBar.kt
+++ app/src/main/java/com/winteryy/readit/ui/home/HomeSearchBar.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -23,7 +21,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.winteryy.readit.R
 import com.winteryy.readit.ui.theme.ReadItTheme
-import com.winteryy.readit.ui.theme.theme_grey_gunPowder
 import com.winteryy.readit.ui.theme.theme_grey_whiteSmoke
 
 @Composable
