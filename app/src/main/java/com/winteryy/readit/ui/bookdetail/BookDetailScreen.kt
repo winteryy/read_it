71152a45673d6784e953067cbfefb85da97afb1b
Lee HuiGwang <lhk9805@gmail.com>
2024-09-05 02:48:33 +0900

    design: 여백 있는 하위 컴포넌트들 배경 White 처리
    issue: #11

--- app/src/main/java/com/winteryy/readit/ui/bookdetail/BookDetailScreen.kt
+++ app/src/main/java/com/winteryy/readit/ui/bookdetail/BookDetailScreen.kt
@@ -1,10 +1,21 @@
 package com.winteryy.readit.ui.bookdetail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.winteryy.readit.model.Book
+import com.winteryy.readit.ui.components.BookItemRow
+import com.winteryy.readit.ui.components.TextTopBar
 import com.winteryy.readit.ui.theme.ReadItTheme
 import java.util.Date
 
@@ -13,7 +24,30 @@ fun BookDetailScreen(
     book: Book,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White) //todo 완성 후 필요 체크
+    ) {
+        val scrollState = rememberScrollState()
 
+        TextTopBar(
+            title = "책 상세 정보"
+        )
+        HorizontalDivider()
+        Column(
+            modifier = Modifier
+                .verticalScroll(scrollState)
+                .background(Color.LightGray)
+        ) {
+            BookItemRow(book = book)
+            Spacer(modifier = Modifier.size(8.dp))
+            BookItemRow(book = book)
+            Spacer(modifier = Modifier.size(8.dp))
+
+        }
+
+    }
 }
 
 @Composable
--- app/src/main/java/com/winteryy/readit/ui/components/TextTopBar.kt
+++ app/src/main/java/com/winteryy/readit/ui/components/TextTopBar.kt
@@ -1,5 +1,6 @@
 package com.winteryy.readit.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +11,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.winteryy.readit.ui.theme.Typography
@@ -24,24 +27,29 @@ fun TextTopBar(
     title: String = "",
     onBackArrowClicked: () -> Unit = {}
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .heightIn(min = 40.dp)
+    Surface(
+        modifier = modifier,
+        color = Color.White
     ) {
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-            contentDescription = "back button",
-            modifier = Modifier.clickable { onBackArrowClicked() }
-        )
-        Spacer(modifier = Modifier.size(16.dp))
-        Text(
-            text = title,
-            style = Typography.headlineSmall.copy(
-                fontWeight = FontWeight.Bold
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .heightIn(min = 40.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "back button",
+                modifier = Modifier.clickable { onBackArrowClicked() }
             )
-        )
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = title,
+                style = Typography.headlineSmall.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
     }
 }
