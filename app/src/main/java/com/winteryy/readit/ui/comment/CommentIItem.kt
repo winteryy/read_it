d83caa1bdc045c12635f677089d2cdef0cf3b50a
Lee HuiGwang <lhk9805@gmail.com>
2024-09-15 22:18:56 +0900

    refactor: CommentItem 구성 ConstraintLayout으로 수정
    issue: #15

--- app/src/main/java/com/winteryy/readit/ui/comment/CommentIItem.kt
+++ app/src/main/java/com/winteryy/readit/ui/comment/CommentIItem.kt
@@ -1,21 +1,19 @@
 package com.winteryy.readit.ui.comment
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.winteryy.readit.model.Book
 import com.winteryy.readit.ui.components.BookItem
 import com.winteryy.readit.ui.theme.DEFAULT_PADDING
@@ -32,58 +30,52 @@ fun CommentItem(
 ) {
     Surface(
         modifier = modifier
+            .width(360.dp)
             .clip(RoundedCornerShape(10.dp)),
         color = theme_grey_whiteSmoke
     ) {
-        Layout(
-            content = {
-                BookItem(
-                    book = book
-                )
-                Column {
-                    Text(
-                        text = "for testfor testfor testfor testfor testfor testfor testfor testforfor testfor testfor testfor testfor testfor testfor testfor testforfor testfor testfor testfor testfor testfor testfor testfor testforfor testfor testfor testfor testfor testfor testfor testfor testfor testfor test",
-                        style = Typography.labelMedium,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier
-                            .weight(1f),
-                    )
-                    Spacer(
-                        modifier = Modifier.size(8.dp)
-                    )
-                    Text(
-                        text = "24.09.23",
-                        style = Typography.labelMedium,
-                        color = theme_color_lightDodgerBlue,
-                        textAlign = TextAlign.Right,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    )
-                }
-            },
+        ConstraintLayout(
             modifier = Modifier
                 .padding(DEFAULT_PADDING)
-        ) { measurables , constraints ->
-            val bookItem = measurables[0]
-            val bookItemPlaceable = bookItem.measure(constraints)
+        ) {
+            val (bookItem, contentText, dateText) = createRefs()
 
-            val maxHeight = bookItemPlaceable.height
-
-            val column = measurables[1]
-            val columnPlaceable = column.measure(
-                constraints.copy(
-                    maxHeight = maxHeight,
-                    maxWidth = constraints.maxWidth-bookItemPlaceable.width-8.dp.roundToPx()
-                )
+            BookItem(
+                book = book,
+                modifier = Modifier
+                    .constrainAs(bookItem) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
             )
-
-            layout(constraints.maxWidth, maxHeight) {
-                bookItemPlaceable.place(0, 0)
-                columnPlaceable.place(bookItemPlaceable.width + 8.dp.roundToPx(), 0)
-            }
+            Text(
+                text = "tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test tfor testfor testfor test ",
+                style = Typography.labelMedium,
+                overflow = TextOverflow.Ellipsis,
+                softWrap = true,
+                modifier = Modifier
+                    .constrainAs(contentText) {
+                        top.linkTo(parent.top)
+                        start.linkTo(bookItem.end, margin = 8.dp)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(dateText.top)
+                        width = Dimension.fillToConstraints
+                        height = Dimension.fillToConstraints
+                    }
+            )
+            Text(
+                text = "24.09.23",
+                style = Typography.labelMedium,
+                color = theme_color_lightDodgerBlue,
+                textAlign = TextAlign.Right,
+                modifier = Modifier
+                    .constrainAs(dateText) {
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(parent.end)
+                    }
+            )
         }
     }
-
 }
 
 @Composable
--- app/src/main/java/com/winteryy/readit/ui/comment/CommentScreen.kt
+++ app/src/main/java/com/winteryy/readit/ui/comment/CommentScreen.kt
@@ -1,9 +1,59 @@
 package com.winteryy.readit.ui.comment
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.winteryy.readit.model.Book
+import com.winteryy.readit.ui.theme.ReadItTheme
+import java.util.Date
 
 @Composable
 fun CommentScreen() {
-    Text(text = "Comment")
+    val bookList = listOf(
+        Book(
+            "정의란 무엇인가",
+            "https://picsum.photos/300/400",
+            "test author",
+            "test publisher",
+            "1321412",
+            description = "asdasd",
+            pubDate = Date(),
+        ),
+        Book(
+            "정의란 무엇인가",
+            "https://picsum.photos/300/400",
+            "test author",
+            "test publisher",
+            "1321412",
+            description = "asdasd",
+            pubDate = Date(),
+        )
+    )
+
+    Column() {
+        Text("최근에 작성한 코멘트")
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            items(
+                count = bookList.size,
+            ) { ind ->
+                CommentItem(book = bookList[ind])
+            }
+        }
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CommentScreenPreview() {
+    ReadItTheme {
+        CommentScreen()
+    }
 }
