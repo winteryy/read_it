33aa895af26ca0b2261f73b7b53b1acad4bb4942
Lee HuiGwang <lhk9805@gmail.com>
2024-09-23 05:10:53 +0900

    feat: CommentViewModel 기본 틀 구현
    issue: #17

--- app/src/main/java/com/winteryy/readit/ui/comment/CommentUiState.kt
+++ app/src/main/java/com/winteryy/readit/ui/comment/CommentUiState.kt
@@ -1,16 +1,19 @@
 package com.winteryy.readit.ui.comment
 
 import androidx.paging.PagingData
+import com.winteryy.readit.model.Book
 import com.winteryy.readit.model.Comment
 import kotlinx.coroutines.flow.Flow
 
 sealed interface CommentUiState {
     object Loading: CommentUiState
+    object FailState: CommentUiState
     data class CommentMainState(
         val commentNum: Int,
         val recentCommentList: List<Comment>
     ): CommentUiState
     data class CommentListState(
-        val commentPagingDataFlow: Flow<PagingData<Comment>>
+        val commentPagingDataFlow: Flow<PagingData<Pair<Comment, Book>>>
     ): CommentUiState
+
 }
--- app/src/main/java/com/winteryy/readit/ui/comment/CommentViewModel.kt
+++ app/src/main/java/com/winteryy/readit/ui/comment/CommentViewModel.kt
@@ -1,13 +1,65 @@
 package com.winteryy.readit.ui.comment
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.winteryy.readit.data.Result
+import com.winteryy.readit.data.local.commentstorage.CommentStorageRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CommentViewModel @Inject constructor(
+    private val commentStorageRepository: CommentStorageRepository
+) : ViewModel() {
+    private val _commentUiState: MutableStateFlow<CommentUiState> = MutableStateFlow(CommentUiState.Loading)
+    val commentUiState: StateFlow<CommentUiState> get() = _commentUiState.asStateFlow()
 
-): ViewModel() {
+    private var currentJob: Job? = null
 
+    init {
+        setCommentMainState()
+    }
 
+    fun setCommentMainState() {
+        currentJob?.cancel()
+
+        currentJob = viewModelScope.launch {
+            commentStorageRepository.getRecentComments()
+                .combine(commentStorageRepository.getCommentNum()) { recentCommentList, commentNum ->
+                    if (recentCommentList is Result.Success && commentNum is Result.Success) {
+                        _commentUiState.value = CommentUiState.CommentMainState(
+                            commentNum.data,
+                            recentCommentList.data
+                        )
+                    } else {
+                        _commentUiState.value = CommentUiState.FailState
+                    }
+                }
+        }
+    }
+
+    fun setCommentListState() {
+        currentJob?.cancel()
+
+        viewModelScope.launch {
+            when(val result = commentStorageRepository.getCommentsWithBooksPagingFlow()) {
+                is Result.Error -> {
+                    //todo 에러 핸들링
+                }
+                is Result.Success -> {
+                    _commentUiState.value = CommentUiState.CommentListState(
+                        result.data
+                    )
+                }
+            }
+        }
+    }
 }
