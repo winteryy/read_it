856710a7176eec53a40baff9b2c46338a1549082
Lee HuiGwang <lhk9805@gmail.com>
2024-10-15 00:49:21 +0900

    feat: 로딩용 컴포넌트 생성
    issue: #19

---
+++ app/src/main/java/com/winteryy/readit/ui/components/IndeterminateCircularIndicator.kt
@@ -0,0 +1,35 @@
+package com.winteryy.readit.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.winteryy.readit.ui.theme.ReadItTheme
+import com.winteryy.readit.ui.theme.theme_color_dodgerBlue
+
+@Composable
+fun IndeterminateCircularIndicator(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = theme_color_dodgerBlue
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun IndeterminateCircularIndicatorPreview() {
+    ReadItTheme {
+        IndeterminateCircularIndicator()
+    }
+}
